6545af1990da7ec4765a8bea0e2c32f0aedb67db
RichardHightower <richard.m.hightower@gmail.com>
2017-07-17 16:50:16 -0300

    Clean temp file script

--- java/clean_temp_file.sh
+++ java/clean_temp_file.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
+
+# Used to clean temp files for zip production 
 find . -name "build" | xargs rm -rf
+find . -name ".gradle" | xargs rm -rf
+find . -name ".idea" | xargs rm -rf
+find . -name ".DS_Store" | xargs rm -rf
